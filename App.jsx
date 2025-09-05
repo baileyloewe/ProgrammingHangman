7110e953db1c53da4c1c3df6e16dbbb863d3a5c3
Bailey Loewe <72360204+baileyloewe@users.noreply.github.com>
2025-09-05 09:55:00 -0400

    Continued to flesh out game with features

--- App.jsx
+++ App.jsx
@@ -1,17 +1,20 @@
 import { languages } from "./languages.js"
 import { useState, useEffect, useRef } from 'react'
 import { nanoid } from "nanoid"
+import { getFarewellText } from "./utils.js"
 
 
 export default function Hangman() {
 
-    const [guessedLetters, setGuessedLetters] = useState(() => new Set())
-    const [word, setWord] = useState(() => "react")
-    const alphabet = "abcdefghijklmnopqrstuvwxyz"
-    const wrongGuessCount = [...guessedLetters].reduce((acc, letter) => !word.includes(letter) ? acc + 1 : acc, 0)
-    const isGameWon = word.split("").every(letter => guessedLetters.has(letter))
-    const isGameLost = wrongGuessCount === languages.length - 1
-    
+  const [guessedLetters, setGuessedLetters] = useState(() => new Set())
+  const [word, setWord] = useState(() => "react")
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+  const wrongGuessCount = [...guessedLetters].reduce((acc, letter) => !word.includes(letter) ? acc + 1 : acc, 0)
+  const isGameWon = word.split("").every(letter => guessedLetters.has(letter))
+  const isGameLost = wrongGuessCount === languages.length - 1
+  const isGameOver = isGameWon || isGameLost
+  const lastGuessedLetter = Array.from(guessedLetters).pop()
+  const isLastGuessIncorrect = Array.from(guessedLetters)[0] && !word.includes(Array.from(guessedLetters).pop())
 
   const languageElements = languages.map((lang, index) => {
     const styles = {
@@ -20,8 +23,6 @@ export default function Hangman() {
     }
     return <span className={`language-chip ${index < wrongGuessCount ? "lost" : ""}`} key={lang.name} style={styles}>{lang.name}</span>
   })
-
-  
 
   const letterElements = word.split('').map(letter => (
     <span className="letter" key={nanoid()}>{guessedLetters.has(letter) ? letter.toUpperCase() : ""}</span>)
@@ -33,12 +34,22 @@ export default function Hangman() {
     const correctStatus = 
         isGuessed && inWord ? "correct" :
         isGuessed && !inWord ? "incorrect" :
-        ""
-    return <button className={`key ${correctStatus}`} onClick={() => addGuessedLetter(letter)}>{letter.toUpperCase()}</button>
+        isGameOver ? "disabled" : ""
+    const disabled = isGameOver ? "disabled" : ""
+    return <button disabled={isGameOver} aria-label={`Letter: ${letter}`}aria-disabled={Array.from(guessedLetters).includes(letter)} className={`key ${correctStatus} ${disabled}`} onClick={() => addGuessedLetter(letter)}>{letter.toUpperCase()}</button>
   })
 
   function addGuessedLetter(letter) {
+    if (!isGameOver)
     setGuessedLetters(prevLetters => new Set(prevLetters).add(letter))
+  }
+
+  function renderGameStatus() {
+    if (!isGameOver && isLastGuessIncorrect) { return <><h2>{getFarewellText(languages[wrongGuessCount - 1].name)}</h2></> }
+    
+    if (isGameWon) { return <><h2>You win!</h2><p>Well done! 🎉</p></> }
+    else if (isGameLost) { return <><h2>Game Over!</h2><p>You lose! Better start learning Assembly!</p></> }
+    else { return <><h2></h2><p></p></> }
   }
 
   return (
@@ -48,19 +59,30 @@ export default function Hangman() {
             <p>Guess the word within 8 attempts to keep the 
             programming world safe from Assembly!</p>
           </header>
-          <section className={`game-status ${isGameWon ? "won" : isGameLost ? "lost" : ""}`}>
-                {
-                isGameWon ?  <><h2>You win!</h2><p>Well done! 🎉</p></> : 
-                isGameLost ? <><h2>Game Over!</h2><p>You lose! Better start learning Assembly!</p></> : 
-                             <><h2></h2><p></p></>
-                }
-            </section>
+          <section className={`game-status ${isGameWon ? "won" : isGameLost ? "lost" : isLastGuessIncorrect ? "farewell" : ""}`} aria-live="polite" role="status">
+            {renderGameStatus()}
+          </section>
           <section className="language-chips">
             {languageElements}
           </section>
           <section className="word">
             {letterElements}
           </section>
+          <section className="sr-only" aria-live="polite" role="status" >
+            <p>
+              {word.includes(lastGuessedLetter) ? 
+                  `Correct! The letter ${lastGuessedLetter} is in the word.` : 
+                  `Sorry, the letter ${lastGuessedLetter} is not in the word.`
+              }
+              You have {word.length - wrongGuessCount} attempts left.
+            </p>
+            <p>Current word: 
+            {word.split("").map(letter => 
+            Array.from(guessedLetters).includes(letter) ? letter + "." : "blank.")
+            .join(" ")}
+            </p>
+            
+            </section>
           <section className="keyboard">
             {keyboardElements}
           </section>
@@ -68,28 +90,3 @@ export default function Hangman() {
       </main>
   )
 }
-
-
-
-/**
- * Project planning:
- * 
- * Questions to ask yourself before writing any code:
- * 
- * - What are the main containers of elements I need
- *   in this app?
- *  I'd do a header for title, instructions, and eventually the win/lost text. 
- *  I'd then have a main component, with 3 child components: langauges (alive and dead),
- * guess text, and then keyboard, then just a button for the new game
- * 
- * - What values will need to be saved in state vs.
- *   what values can be derived from the state?
- * 
- * guesses and alive/dead languages will need to be saved in state, winning state can be derived. 
- * 
- * - How will the user interact with the app? What
- *   events do I need to handle?
- * 
- * they will click on the letters, so we need to handle that event. they will also click
- * on new game
- */
