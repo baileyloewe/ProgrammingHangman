bf703bba169e481f8175adf4139acf092db6822d
Bailey Loewe <72360204+baileyloewe@users.noreply.github.com>
2025-09-05 22:26:25 -0400

    Updated title header

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ export default function Hangman() {
   return (
       <main>
           <header>
-            <h1>Assembly: Endgame</h1>
+            <h1>Programming Hangman</h1>
             <p>Guess the word within 8 attempts to keep the 
             programming world safe from Assembly!</p>
           </header>
