4fd3a5389879d3b25416b9aa30ba779d9629a7d8
Bailey Loewe <72360204+baileyloewe@users.noreply.github.com>
2025-09-06 16:55:17 -0400

    Create App.test.jsx

---
+++ src/components/App.test.jsx
@@ -0,0 +1,46 @@
+import { vi, describe, test, expect } from 'vitest'
+import { render, screen} from '@testing-library/react'
+
+import App from '/src/components/App.jsx'
+import * as utils from '/src/utils/utils.js'
+
+
+describe("App", () => {
+    test("Displays app name", () => {
+        render(<App />)
+        expect(screen.getByText("Programming Hangman")).toBeInTheDocument()
+    })
+
+    test("Displays instructions", () => {
+        render(<App />)
+        expect(screen.getByText("Guess the word within 8 attempts to keep the programming world safe from Assembly!")).toBeInTheDocument()
+    })
+
+    test("Displays langauages", () => {
+        render(<App />)
+        expect(screen.getByText("HTML")).toBeInTheDocument()
+        expect(screen.getByText("CSS")).toBeInTheDocument()
+        expect(screen.getByText("JavaScript")).toBeInTheDocument()
+        expect(screen.getByText("React")).toBeInTheDocument()
+        expect(screen.getByText("TypeScript")).toBeInTheDocument()
+        expect(screen.getByText("Node.js")).toBeInTheDocument()
+        expect(screen.getByText("Python")).toBeInTheDocument()
+        expect(screen.getByText("Ruby")).toBeInTheDocument()
+        expect(screen.getByText("Assembly")).toBeInTheDocument()
+    })
+
+    test("Displays correct number of letters", () => {
+        vi.spyOn(utils, 'getRandomWord').mockReturnValue('testing')
+        render(<App />)
+        const letterElements = screen.getAllByLabelText('letter')
+        expect(letterElements).toHaveLength(7)
+    })
+
+    test("Displays full keyboard", () => {
+        const { container } = render(<App />)
+        const keyboardButtons = container.querySelectorAll('.key')
+        expect(keyboardButtons).toHaveLength(26)
+    })
+
+
+})
